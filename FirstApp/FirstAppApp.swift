b9d182a277f1d0e969d0f27cedb0feef5c8da9c8
Fabien Bouteille <fabien.bouteille@icloud.com>
2022-01-19 09:44:34 -0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Fabien Bouteille on 2022-01-19.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
